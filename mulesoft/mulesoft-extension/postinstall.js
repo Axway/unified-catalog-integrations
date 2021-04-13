30a97213867b36b6bead9a5cd772d0d4e5a957d6
Seth Toda <stoda@axway.com>
2021-04-13 16:51:10 -0600

    APIGOV-18261: fix all postinstall scripts

--- mulesoft/mulesoft-extension/postinstall.js
+++ mulesoft/mulesoft-extension/postinstall.js
@@ -24,5 +24,5 @@ const info = (header, example) => {
 };
 info(
 	'Extension Installed: You may now add it to the Central CLI\n',
-	`$ amplify central config set extensions.mulesoft-extension ${process.cwd()}${path.sep}node_modules${path.sep}@axway${path.sep}amplify-central-mulesoft-extension`
+	`$ amplify central config set extensions.mulesoft-extension ${process.cwd()}`
 );
